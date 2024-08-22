492330a8728df59786f878f2cbb0b41e98274029
nick <113468029+ugownqvckflq@users.noreply.github.com>
2024-08-22 17:19:56 +0300

    добавил аспект на проверку ролей

---
+++ role-check-lib/src/main/java/com/project/rolechecker/RoleCheckConfig.java
@@ -0,0 +1,11 @@
+package com.project.rolechecker;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan(basePackages = "com.project.rolechecker")
+public class RoleCheckConfig {
+}
